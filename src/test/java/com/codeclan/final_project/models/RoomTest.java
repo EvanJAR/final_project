0322e09cc30b5a90178ebc77b5d221897d9a3c42
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-17 11:25:15 +0000

    Added test files for models

---
+++ src/test/java/com/codeclan/final_project/models/RoomTest.java
@@ -0,0 +1,12 @@
+package com.codeclan.final_project.models;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RoomTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
