19fc7640be633a7b6105080e84bcd18a82946f6f
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-17 14:43:59 +0000

    Added to basket class - id, annotations, basket property to constructor

--- src/main/java/com/codeclan/final_project/models/House.java
+++ src/main/java/com/codeclan/final_project/models/House.java
@@ -1,14 +1,35 @@
 package com.codeclan.final_project.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "houses")
 public class House {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String houseName;
+
+    @Column
+    private Basket basket;
+
+    @OneToMany(mappedBy = "house")
+    @JsonIgnoreProperties({"house"})
     private ArrayList<Room> rooms;
 
-    public House(){
+    public House(String houseName, Basket basket){
+        this.houseName = houseName;
+        this.basket = basket;
         this.rooms = new ArrayList<>();
     }
+
+    public House(){};
 
     public ArrayList<Room> getRooms() {
         return rooms;
@@ -28,5 +49,21 @@ public class House {
 
     public void removeRoom(Room room) {
         rooms.remove(room);
+    }
+
+    public String getHouseName() {
+        return houseName;
+    }
+
+    public void setHouseName(String houseName) {
+        this.houseName = houseName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
--- src/test/java/com/codeclan/final_project/models/HouseTest.java
+++ src/test/java/com/codeclan/final_project/models/HouseTest.java
@@ -1,5 +1,6 @@
 package com.codeclan.final_project.models;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,19 +9,32 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class HouseTest {
 
+    Basket basket;
+
+    @Before
+    public void before(){
+        basket = new Basket();
+    }
+
     @Test
     public void contextLoads() {
     }
 
     @Test
+    public void houseHasName(){
+        House house = new House("Camerons House", basket);
+        assertEquals("Camerons House", house.getHouseName());
+    }
+
+    @Test
     public void houseStartsWithEmptyListOfRooms(){
-        House house = new House(){};
+        House house = new House("Camerons House", basket){};
         assertEquals(0, house.getNumberOfRooms());
     }
 
     @Test
     public void canAddRoomToHouse(){
-        House house = new House(){};
+        House house = new House("Camerons House", basket){};
         Room room = new Room("Kitchen", RoomType.KITCHEN);
         house.addRoom(room);
         assertEquals(1, house.getNumberOfRooms());
@@ -28,7 +42,7 @@ public class HouseTest {
 
     @Test
     public void canDeleteRoom(){
-        House house = new House(){};
+        House house = new House("Camerons House", basket){};
         Room room = new Room("Kitchen", RoomType.KITCHEN);
         house.addRoom(room);
         house.removeRoom(room);
