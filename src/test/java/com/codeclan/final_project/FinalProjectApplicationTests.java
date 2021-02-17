d7de1dedba54692153f82c0b56724eb5ef9e31d2
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-17 15:14:48 +0000

    Changed main testing file to normal testing import (not jupiter)

--- src/test/java/com/codeclan/final_project/FinalProjectApplicationTests.java
+++ src/test/java/com/codeclan/final_project/FinalProjectApplicationTests.java
@@ -1,13 +1,13 @@
 package com.codeclan.final_project;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FinalProjectApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 
