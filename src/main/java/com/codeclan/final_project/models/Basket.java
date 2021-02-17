12900cb309311529bf9664ff4a14a741f876f76a
Cameron Maclean <cm.maclean@yahoo.com>
2021-02-17 15:08:13 +0000

    Added to basket class - added annotations to properties

--- src/main/java/com/codeclan/final_project/models/Basket.java
+++ src/main/java/com/codeclan/final_project/models/Basket.java
@@ -1,9 +1,20 @@
 package com.codeclan.final_project.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 
+@Entity
+@Table(name = "baskets")
 public class Basket {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(mappedBy = "basket")
+    @JsonIgnoreProperties({"basket"})
     private ArrayList<Item> basketItems;
 
     public Basket(){
@@ -34,5 +45,13 @@ public class Basket {
 
         return basketItems
                 .stream().mapToDouble(Item::getPrice).sum();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
