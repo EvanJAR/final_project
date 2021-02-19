55abbb03615c8bd3f9c8f3d2e536a08dc3ad094f
Evan Reid <evan_1995@hotmail.com>
2021-02-19 16:09:40 +0000

    getBasketsById added to controller and is working

--- src/main/java/com/codeclan/final_project/controllers/BasketController.java
+++ src/main/java/com/codeclan/final_project/controllers/BasketController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BasketController {
@@ -20,5 +22,10 @@ public class BasketController {
     public ResponseEntity<List<Basket>> getAllBaskets(){
         List<Basket> allBaskets = basketRepository.findAll();
         return new ResponseEntity<>(allBaskets, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/baskets/{id}")
+    public ResponseEntity<Optional<Basket>> getBasketById(@PathVariable Long id){
+        return new ResponseEntity<>(basketRepository.findById(id), HttpStatus.OK);
     }
 }
