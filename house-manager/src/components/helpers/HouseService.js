5e118aacd0132096039a0fee4970592c50afa2cc
Evan Reid <evan_1995@hotmail.com>
2021-02-21 15:37:58 +0000

    stuck with the data fetch

---
+++ house-manager/src/components/helpers/HouseService.js
@@ -0,0 +1,6 @@
+// const housesURL = 'http://localhost:8080/houses';
+
+// export const getAllHouses = () => {
+//     return fetch(housesURL)
+//     .then(res => res.json())
+// };
--- house-manager/src/components/houseComponents/HouseList.js
+++ house-manager/src/components/houseComponents/HouseList.js
@@ -1,12 +1,32 @@
 import House from "../../containers/House";
+import {useState, useEffect} from 'react';
+import {getAllHouses} from '../helpers/HouseService';
 
 function HouseList(){
+
+  const [houses, setHouses] = useState([]);
+
+  
+  const getAllHouses = () => {
+    fetch('http://localhost:8080/houses')
+    .then(res => res.json())
+    .then(data => setHouses(data));
+  }
+
+  useEffect(() => {
+    getAllHouses();
+  }, [])
+
+
+
+console.log("console of houses outside", houses)
 
   return(
     <>
       <h1>This is a list of house objects</h1>
       {/* Here we do a fetch to the database to find houses that already exist */}
       {/* House 1 - on click re-route to "houses/{1}" */}
+      
     </>
   )
 }
